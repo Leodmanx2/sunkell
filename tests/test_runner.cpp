65ff30f1b49de6efcefd7eda699a304e76c71e1f
Chris MacLeod <chris@chris-macleod.ca>
2023-04-18 21:59:13 -0400

    Initial commit

---
+++ tests/test_runner.cpp
@@ -0,0 +1,10 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+TEST_CASE("doctest is integrated") {
+	// This empty test will automatically pass.
+}
