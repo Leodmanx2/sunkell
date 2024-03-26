d9297c18dea53408c5651de6e2b397a2f8ca835c
Chris MacLeod <chris@chris-macleod.ca>
2024-03-26 16:42:03 -0400

    Remove non-functional test
    It looks like we're going to have to write sample application and use actual user inputs in order to test the input queue.

--- tests/input_test.cpp
+++ tests/input_test.cpp
@@ -11,29 +11,3 @@
 #endif // SUNKELL_PLATFORM_WIN32
 
 using namespace sunkell;
-
-TEST_SUITE("input") {
-#ifdef SUNKELL_PLATFORM_WIN32
-	TEST_CASE("input event triggers callback") {
-		input_event_queue                queue;
-		const std::shared_ptr<key_event> trigger_event =
-		  std::make_shared<key_event>(button::a, button_state::down);
-		int  i        = 0;
-		auto callback = [&i](std::shared_ptr<event> e) -> void { i = 1; };
-		auto receipt  = queue.register_callback(trigger_event, callback);
-
-		// Simulate keyboard input
-		INPUT input;
-		input.type           = INPUT_KEYBOARD;
-		input.ki.wVk         = 'A';
-		input.ki.dwFlags     = 0;
-		input.ki.time        = 0;
-		input.ki.dwExtraInfo = 0;
-		SendInput(1, &input, sizeof(INPUT));
-
-		queue.poll();
-		queue.dispatch_next_event();
-		REQUIRE(i == 1);
-	}
-#endif // SUNKELL_PLATFORM_WIN32
-}
